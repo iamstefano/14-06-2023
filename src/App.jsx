60158aa20ffa013de1c8c8b8ff7d322b3a4307d3
Iamstefanoe <stefanoar3@gmail.com>
2023-06-14 17:55:25 +0200

    14-06-2023 Esercizio 1 e Avanzato

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Navbar from "./components/navbar";
+import Title from "./components/title";
 import Todo from "./components/todo";
+/* import Element from "./components/element"; */
 import { todos } from "./mocks/todos";
 import "./App.css";
 
@@ -7,14 +9,8 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>My first react app is a to-do list</h1>
-      <div className="App">
-        <div className="App__todosList ">
-          {todos.map((todo) => (
-            <Todo key={todo.id} todo={todo} />
-          ))}
-        </div>
-      </div>
+      <Title />
+      <Todo />
     </div>
   );
 }
